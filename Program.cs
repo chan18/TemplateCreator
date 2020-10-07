7c51c9dd3e6c5b0a862844355a294d102a625f41
Chandrakanth <cpasunuru@saventech.com>
2020-10-07 19:01:01 +0530

    init commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace EmailConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"Config.json";
+
+            string folderName = @"Temp";
+
+            if (!System.IO.Directory.Exists(folderName))
+            {
+                System.IO.Directory.CreateDirectory(folderName);
+            }
+
+            JArray tempalteConfig  = JArray.Parse(File.ReadAllText(filePath));
+
+            foreach(var config in tempalteConfig)
+            {
+                string clientId = config["client"].ToString();
+
+                foreach (var status in config["statuses"])
+                {
+                    foreach(var program in config["programs"])
+                    {
+                        foreach(var language in config["languages"])
+                        {
+                            string fileName = $"{clientId}_{program.ToString().Replace(" ","_")}_{status.ToString()}_{language.ToString()}";
+                            string pathString = System.IO.Path.Combine(folderName, fileName);
+                            System.IO.File.Create(pathString);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
